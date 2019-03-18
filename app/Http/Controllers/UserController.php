a45b536f365596f8b67402d43458f48946887d92
Azmir Shah <azmirshah1@gmail.com>
2019-03-18 23:32:31 +0800

    Update 3.1 -Avatar - completed
    Complete adding avatar feature.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,16 +14,16 @@ class UserController extends Controller
     	return view('profile', array ('user'=> Auth::user()));
     }
 
-        public function updateAvatar(request $request){
-    	//handle the user upload of avatar
-        	if($request->hasFile('avatar')){
-        		$avatar=$request->file('avatar');
-        		$filename=time() . '.' . $avatar->getClientOriginalExtension();
-        		Image::make($avatar)->resize(300,300)->save(public_path('/assets/images/avatar/' . $filename));
-        		$user = Auth::user();
-        		$user->avatar = $filename;
-        		$user->save();
-        	}
-        	return view('profile', array ('user'=> Auth::user()));
+    public function updateAvatar(request $request){
+	//handle the user upload of avatar
+    	if($request->hasFile('avatar')){
+    		$avatar=$request->file('avatar');
+    		$filename=time() . '.' . $avatar->getClientOriginalExtension();
+    		Image::make($avatar)->resize(300,300)->save(public_path('/assets/images/avatar/' . $filename));
+    		$user = Auth::user();
+    		$user->avatar = $filename;
+    		$user->save();
+    	}
+    	return view('profile', array ('user'=> Auth::user()));
     }
 }
