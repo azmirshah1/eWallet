1bf55cdfe263c2a9cf74e474aef7d52576b75f62
Azmir Shah <azmirshah1@gmail.com>
2019-03-19 10:17:19 +0800

    update 3.1 - update design2.blade.php

--- resources/views/layouts/design2.blade.php
+++ resources/views/layouts/design2.blade.php
@@ -67,7 +67,7 @@
             </div>
             <div class="col-sm-6 clearfix">
                 <div class="user-profile pull-right">
-                    <img class="avatar user-thumb" src="assets/images/author/avatar.png" alt="avatar">
+                    <img class="avatar user-thumb" src="assets/images/avatar/{{ Auth::user()->avatar}}" alt="avatar">
                     <h4 class="user-name dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}<i class="fa fa-angle-down"></i></h4>
                     <div class="dropdown-menu">
                         <a class="dropdown-item" href="#">Message</a>
