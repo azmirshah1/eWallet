69bf6ac9361c169931fe4dc0ed37528aa808660d
Azmir Shah <azmirshah1@gmail.com>
2019-03-26 10:22:59 +0800

    update 3.3 - improvise loading API
    To keep API under limit, I have put it into another files.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,84 +4,86 @@
 <div class="col-lg-12 col-sm-6 text-center mb-4">
     <!-- sales report area start -->
     <hr style="border-color: #9e0039;">
-        <div class="sales-report-area mt-5 mb-5 ">
-            <div class="row">
-                {{-- bitcoin --}}
-                <div class="col-md-4">
-                    <div class="single-report mb-xs-30">
-                        <div class="s-report-inner pr--20 pt--30 mb-3">
-                            <div class="icon"><i class="fa fa-btc"></i></div>
-                            <div class="s-report-title d-flex justify-content-between">
-                                <h4 class="header-title mb-0">Bitcoin (BTC)</h4>
-                                <p id="hours"></p>
-                            </div>
-                            <div class="d-flex justify-content-between pb-2">
-                                <h2 id="BTCRates"></h2>
-                                <span>- 45.87</span>
-                            </div>
-                        </div>
-                        <canvas id="coin_sales1" height="100"></canvas>
-                    </div>
-                </div>
-                {{-- bitcoin cash--}}
-                <div class="col-md-4">
-                    <div class="single-report mb-xs-30">
-                        <div class="s-report-inner pr--20 pt--30 mb-3">
-                            <div class="icon"><i class="fa fa-btc"></i></div>
-                            <div class="s-report-title d-flex justify-content-between">
-                                <h4 class="header-title mb-0">Etherium (ETH)</h4>
-                                <p id="hours1"></p>
-                            </div>
-                            <div class="d-flex justify-content-between pb-2">
-                                <h2 id="ETHRates"></h2>
-                                <span>- 45.87</span>
-                            </div>
-                        </div>
-                        <canvas id="coin_sales2" height="100"></canvas>
-                    </div>
-                </div>
-                {{-- Etherium --}}
-                <div class="col-md-4">
-                    <div class="single-report">
-                        <div class="s-report-inner pr--20 pt--30 mb-3">
-                            <div class="icon"><i class="fa fa-eur"></i></div>
-                            <div class="s-report-title d-flex justify-content-between">
-                                <h4 class="header-title mb-0">Cardano</h4>
-                                <p id="hours2"></p>
-                            </div>
-                            <div class="d-flex justify-content-between pb-2">
-                                <h2 id="ADARates"></h2>
-                                <span>- 45.87</span>
-                            </div>
-                        </div>
-                        <canvas id="coin_sales3" height="100"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- sales report area end -->
-
-        <!-- Team Members Row -->
+    <div class="sales-report-area mt-5 mb-5 ">
         <div class="row">
-            <div class="col-lg-12">
-              <h2 class="my-4"><br></h2>
+            {{-- bitcoin --}}
+            <div class="col-md-4">
+                <div class="single-report mb-xs-30">
+                    <div class="s-report-inner pr--20 pt--30 mb-3">
+                        <div class="icon"><i class="fa fa-btc"></i></div>
+                        <div class="s-report-title d-flex justify-content-between">
+                            <h4 class="header-title mb-0">Bitcoin (BTC)</h4>
+                            <p id="hours"></p>
+                        </div>
+                        <div class="d-flex justify-content-between pb-2">
+                            <h2 id="BTCRates"></h2>
+                            <span>- 45.87</span>
+                        </div>
+                    </div>
+                    <canvas id="coin_sales1" height="100"></canvas>
+                </div>
             </div>
-            <div class="col-lg-4 col-sm-6 text-center mb-4">
-              <a href="manageusers"><img class="rounded-circle img-fluid d-block mx-auto" src="assets/images/mainbut/people.png" alt="">
-              <h3>Manage Users</a>
-              </h3>
+            {{-- bitcoin cash--}}
+            <div class="col-md-4">
+                <div class="single-report mb-xs-30">
+                    <div class="s-report-inner pr--20 pt--30 mb-3">
+                        <div class="icon"><i class="fa fa-btc"></i></div>
+                        <div class="s-report-title d-flex justify-content-between">
+                            <h4 class="header-title mb-0">Etherium (ETH)</h4>
+                            <p id="hours1"></p>
+                        </div>
+                        <div class="d-flex justify-content-between pb-2">
+                            <h2 id="ETHRates"></h2>
+                            <span>- 45.87</span>
+                        </div>
+                    </div>
+                    <canvas id="coin_sales2" height="100"></canvas>
+                </div>
             </div>
-            <div class="col-lg-4 col-sm-6 text-center mb-4">
-              <a href="/cryptoexchange"><img class="rounded-circle img-fluid d-block mx-auto" src="assets/images/mainbut/crypto.png" alt="">
-              <h3>Crypto Exchange </a>
-              </h3>
-            </div>
-            <div class="col-lg-4 col-sm-6 text-center mb-4">
-              <a href="/setting"><img class="rounded-circle img-fluid d-block mx-auto" src="assets/images/mainbut/setting.png" alt="">
-              <h3>Setting</a>
-              </h3>
+            {{-- Etherium --}}
+            <div class="col-md-4">
+                <div class="single-report">
+                    <div class="s-report-inner pr--20 pt--30 mb-3">
+                        <div class="icon"><i class="fa fa-eur"></i></div>
+                        <div class="s-report-title d-flex justify-content-between">
+                            <h4 class="header-title mb-0">Cardano</h4>
+                            <p id="hours2"></p>
+                        </div>
+                        <div class="d-flex justify-content-between pb-2">
+                            <h2 id="ADARates"></h2>
+                            <span>- 45.87</span>
+                        </div>
+                    </div>
+                    <canvas id="coin_sales3" height="100"></canvas>
+                </div>
             </div>
         </div>
-        <!-- Team Members Row -->
     </div>
+    <!-- sales report area end -->
+
+    <!-- Team Members Row -->
+    <div class="row">
+        <div class="col-lg-12">
+          <h2 class="my-4"><br></h2>
+        </div>
+        <div class="col-lg-4 col-sm-6 text-center mb-4">
+          <a href="manageusers"><img class="rounded-circle img-fluid d-block mx-auto" src="assets/images/mainbut/people.png" alt="">
+          <h3>Manage Users</a>
+          </h3>
+        </div>
+        <div class="col-lg-4 col-sm-6 text-center mb-4">
+          <a href="/cryptoexchange"><img class="rounded-circle img-fluid d-block mx-auto" src="assets/images/mainbut/crypto.png" alt="">
+          <h3>Crypto Exchange </a>
+          </h3>
+        </div>
+        <div class="col-lg-4 col-sm-6 text-center mb-4">
+          <a href="/setting"><img class="rounded-circle img-fluid d-block mx-auto" src="assets/images/mainbut/setting.png" alt="">
+          <h3>Setting</a>
+          </h3>
+        </div>
+    </div>
+    <!-- Team Members Row -->
+</div>
+{{-- save the api.they got limit 500 usage --}}
+{{-- <script src="{{ asset('assets/js/api.js') }}"></script> --}}
 @endsection
--- resources/views/layouts/design.blade.php
+++ resources/views/layouts/design.blade.php
@@ -126,44 +126,7 @@
     <script src="{{ asset('assets/js/jquery.slimscroll.min.js') }}"></script>
     <script src="{{ asset('assets/js/jquery.slicknav.min.js') }}"></script>
 
-    <script type="text/javascript">
 
-    const request = new XMLHttpRequest();
-     //save api using - just enable when u need it
-    // request.open('GET', 'http://api.coinlayer.com/api/live?access_key=022c4f9851295e8e3491e702e6189ac8');
-    request.send(); 
-     
-    request.onload = () => {
-        //BTC
-        var BTCRates = JSON.parse(request.response).rates.BTC;
-        var USDMYR = BTCRates * 4.069;
-        var BTCMYR = USDMYR.toFixed(3);
-        //ETH
-        var ETHRates = JSON.parse(request.response).rates.ETH;
-        var USDMYR = ETHRates * 4.069;
-        var ETHMYR = USDMYR.toFixed(3);
-        //ADA
-        var ADARates = JSON.parse(request.response).rates.ADA;
-        var USDMYR = ADARates * 4.069;
-        var ADAMYR = USDMYR.toFixed(3);
-
-        var timestamp = JSON.parse(request.response).timestamp;
-        var date = new Date(timestamp*1000);
-
-        
-        document.getElementById("BTCRates").innerHTML="$ MYR " + BTCMYR;
-        document.getElementById("ETHRates").innerHTML="$ MYR " + ETHMYR;
-        document.getElementById("ADARates").innerHTML="$ MYR " + ADAMYR;
-        // document.getElementById("hours").innerHTML=hours + ":" + minutes + " GMT";
-        document.getElementById("hours").innerHTML=date.toLocaleTimeString("en-US", {hour: 'numeric', minute:'numeric'});
-                document.getElementById("hours1").innerHTML=date.toLocaleTimeString("en-US", {hour: 'numeric', minute:'numeric'});
-                        document.getElementById("hours2").innerHTML=date.toLocaleTimeString("en-US", {hour: 'numeric', minute:'numeric'});
-
-
-    };
-
-
-    </script>
 
     <!-- start chart js -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
